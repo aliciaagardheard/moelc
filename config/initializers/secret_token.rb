19126ece29733276a6360470b5315b1be9eee2a4
Alicia Agard Heard <aagardheard@lat-aagardheard.local>
2014-04-17 11:39:41 -0400

    Initialize repository

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Moelc::Application.config.secret_key_base = '4a98d97c49bbac40cf6fade7411c8e143e65fbae9aa12ab711a136ae7bc75392af04f6ea68352b864994094425787477cb985f2a35aff7aa3d7f69c20bd8e0fe'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Moelc::Application.config.session_store :cookie_store, key: '_moelc_session'
